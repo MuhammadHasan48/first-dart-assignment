a6460449ee0543222a8da0e9d53ee0a37fdf1f3e
MuhammadHasan48 <muhammadhasan4823@gmail.com>
2023-12-31 23:28:44 +0500

    my first dart assignment

---
+++ main.dart
@@ -0,0 +1,199 @@
+
+
+// // assignment 01
+// import 'dart:math';
+
+// void main(){
+
+// // Question 01
+// int Length = 20;
+// int breadth = 20;
+
+// if(Length == breadth){
+//   print("they are square");
+// }
+// else{
+//   print("they are rectangle");
+// }
+
+
+
+// // Question 02
+// int age1 = 18;
+// int age2 = 24;
+
+// if(age1 > age2){
+
+//   print("age1 is older than age2");
+// }
+// else{
+//   print("age2 is older than age1");
+
+// }
+
+
+// // Question 03
+// int classesheld = 16;
+// int classesattend =10;
+
+// num std_attendance_percentage = (classesattend/classesheld)*100;
+// print("student attendance percentage is $std_attendance_percentage");
+
+// if(std_attendance_percentage >= 75){
+//   print("student allow to sit in exam");
+// }
+// else{
+//    print("student will not allow to sit in exam");
+// }
+
+
+
+// // Question 04
+// num Celsius = 32;
+// num Fahrenheit = (Celsius * 9/5) + 32;
+
+// print("$Celsius °C Celsius in Fahrenheit is equal to $Fahrenheit °F");
+
+
+
+
+// // Question 05
+// num temperature = 42;
+
+// if(temperature <= 0){
+//   print("temperature is Freezing");
+// }
+// else if(temperature <= 10 && temperature > 0){
+//   print("temperature is Very Cold");
+
+// }
+// else if(temperature <= 20 && temperature > 10){
+//   print("temperature is Cold");
+
+// }
+// else if(temperature <= 30 && temperature > 20){
+//   print("temperature is Normal");
+
+// }
+// else if(temperature <= 40 && temperature > 30){
+//   print("temperature is Hot");
+
+// }
+// else{
+//   print("temperature is Very Hot");
+// }
+
+
+
+// // Question 06
+
+// String Alphabet = "I";
+
+// if(Alphabet.toLowerCase() == "a"||
+// Alphabet.toLowerCase() == "e" ||
+// Alphabet.toLowerCase() == "i" ||
+// Alphabet.toLowerCase() == "o" ||
+// Alphabet.toLowerCase() == "u"
+// ){
+//   print("$Alphabet is a vowel");
+
+// }
+// else{
+//   print("$Alphabet is a consonantn");
+// }
+
+
+
+// // Question 07
+
+// num number = 25;
+
+// num root = pow(number, 1/2);
+// print(root);
+
+
+// // Question 08
+
+// String stdname = "Muhammad Hasan";
+// String Class = "12";
+// int rollnumber = 001;
+// String Grade;
+
+// int subject1Marks = 82;
+// int subject2Marks = 74;
+// int subject3Marks = 85;
+// int subject4Marks = 76;
+// int subject5Marks = 79;
+
+// int totalMarks = subject1Marks + subject2Marks + subject3Marks + subject4Marks + subject5Marks;
+
+// double percentage = (totalMarks/500)*100;
+// print(percentage);
+
+// if(percentage >= 80 && percentage <=100){
+//  Grade = "A+";
+// }
+// else if(percentage >= 70 && percentage < 80){
+//  Grade = "A";
+// }
+// else if(percentage >= 60 && percentage < 70){
+//  Grade = "B";
+// }
+// else if(percentage >= 50 && percentage < 60){
+//  Grade = "C";
+// }
+// else if(percentage >= 40 && percentage < 50){
+//  Grade = "D";
+// }
+// else{
+//   Grade = "F";
+// }
+
+
+//  print("Marksheet");
+//   print("---------");
+//   print("Student Name: $stdname");
+//   print("Roll Number: $rollnumber");
+//   print("Class: $Class");
+//   print("-----------------");
+//   print("Subject 1: $subject1Marks");
+//   print("Subject 2: $subject2Marks");
+//   print("Subject 3: $subject3Marks");
+//   print("Subject 4: $subject4Marks");
+//   print("Subject 5: $subject5Marks");
+//   print("-----------------");
+//   print("Total Marks: $totalMarks");
+//   print("Percentage: ${percentage.toStringAsFixed(2)}%");
+//   print("Grade: $Grade");
+
+
+// // Question 09
+
+// int numb = 10;
+
+// if(numb%2 == 0){
+//   print("even");
+//   if(numb%5 == 0){
+//     print("Number is Divisible by 5");
+//   }
+//   else{
+//     print("Number is not Divisible by 5");
+
+//   }
+// }
+// else{
+//   print("odd");
+//   if(numb%7 == 0){
+//     print("Number is Divisible by 7");
+//   }
+//   else{
+//     print("Number is not Divisible by 7");
+
+//   }
+
+// }
+
+
+// }
+
+ 
---
+++ main1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void main(){
+// var input = stdin.readLineSync();
+//   print("Enter first numbers:");
+//  int num1 = int.parse(input);
+//   print(num1);
+  // print("Enter second numbers:");
+  // int num2 = int.parse(stdin.readLineSync());
+  // print("Enter third numbers:");
+  // int num3 = int.parse(stdin.readLineSync());
+
+int n = int.parse(stdin.readLineSync());
+
+}
