667d9ad30df155fec8a44c440effe51e3ff5500b
MuhammadHasan48 <155249783+MuhammadHasan48@users.noreply.github.com>
2024-01-01 00:11:08 +0500

    Add files via upload

---
+++ main2.dart
@@ -0,0 +1,17 @@
+void main(){
+var a= 35;
+var b=25;
+if(a<=b){
+  if(a==b){
+   print("equal");
+  }
+  else{
+    print('less than');
+  }
+}
+else{
+  if(a>b){
+    print("greater");
+  }else{print('not equal');}
+}
+}
